0bac753d7e66dce0b367c595026c185816043bac
Ryan Leary <ryanleary@gmail.com>
2017-06-19 08:15:21 -0400

    Remove vestigial comment

--- pytorch_ctc/src/cpu_binding.h
+++ pytorch_ctc/src/cpu_binding.h
@@ -1,4 +1,2 @@
 int ctc_beam_decode(THFloatTensor *probs, THIntTensor *seq_len, THIntTensor *output,
                     THFloatTensor *scores, int top_paths, int beam_width, int merge_repeated);
-
-//int test_ctc_beam_decode();
