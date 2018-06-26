46034dcc79bded7ddfb839ca07e9ec9b0aa257bd
sean.narenthiran <sean.narenthiran@digitalreasoning.com>
2018-06-26 14:15:11 +0100

    Fixed link to boost library, updated to 67_0

--- build.py
+++ build.py
@@ -24,8 +24,8 @@ def download_extract(url, dl_path):
 # Download/Extract openfst, boost
 download_extract('https://sites.google.com/site/openfst/home/openfst-down/openfst-1.6.7.tar.gz',
                  'third_party/openfst-1.6.7.tar.gz')
-download_extract('https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.gz',
-                 'third_party/boost_1_63_0.tar.gz')
+download_extract('https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz',
+                 'third_party/boost_1_67_0.tar.gz')
 
 for file in ['third_party/kenlm/setup.py', 'third_party/ThreadPool/ThreadPool.h']:
     if not os.path.exists(file):
@@ -55,7 +55,7 @@ if compile_test('lzma.h', 'lzma'):
     compile_args.append('-DHAVE_XZLIB')
     ext_libs.append('lzma')
 
-third_party_libs = ["kenlm", "openfst-1.6.7/src/include", "ThreadPool", "boost_1_63_0", "utf8"]
+third_party_libs = ["kenlm", "openfst-1.6.7/src/include", "ThreadPool", "boost_1_67_0", "utf8"]
 compile_args.extend(['-DINCLUDE_KENLM', '-DKENLM_MAX_ORDER=6'])
 lib_sources = glob.glob('third_party/kenlm/util/*.cc') + glob.glob('third_party/kenlm/lm/*.cc') + glob.glob(
     'third_party/kenlm/util/double-conversion/*.cc') + glob.glob('third_party/openfst-1.6.7/src/lib/*.cc')
