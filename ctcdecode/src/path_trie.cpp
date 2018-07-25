d684a46772a7ded2daf87e7491815a1df56f2fc1
sean.narenthiran <sean.narenthiran@digitalreasoning.com>
2018-07-25 16:09:19 +0100

    Added missing paddle changes

--- ctcdecode/src/path_trie.cpp
+++ ctcdecode/src/path_trie.cpp
@@ -53,7 +53,7 @@ PathTrie* PathTrie::get_path_trie(int new_char, int new_timestep, bool reset) {
   } else {
     if (has_dictionary_) {
       matcher_->SetState(dictionary_state_);
-      bool found = matcher_->Find(new_char);
+      bool found = matcher_->Find(new_char + 1);
       if (!found) {
         // Adding this character causes word outside dictionary
         auto FSTZERO = fst::TropicalWeight::Zero();
